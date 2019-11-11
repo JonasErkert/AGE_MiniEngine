6cd647e46b22ed928b7fc890843ee33076290cb6
Jonas Laptop <jonas.erkert@t-online.de>
2019-11-11 17:42:04 +0100

    Corrected errors

--- MiniEngine/shaders/pixelshader.hlsl
+++ MiniEngine/shaders/pixelshader.hlsl
@@ -1,7 +1,7 @@
 struct PS_Input
 {
 	float4 f4Position : SV_Position;
-	float4 f4Color : ASDF;
+	float4 f4Color : ASFD;
 };
 
 float4 main(PS_Input input) : SV_TARGET
--- MiniEngine/shaders/vertexshader.hlsl
+++ MiniEngine/shaders/vertexshader.hlsl
@@ -10,47 +10,47 @@ VS_Output main(uint id : SV_VertexID)
 	if (id == 0)
 	{
 		output.f4Position = float4(-0.5f, -0.3f, 0.0f, 1.0f); // Lower left vertex
-		output.f4Position = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
+		output.f4Color = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
 	}
 	else if (id == 1)
 	{
 		output.f4Position = float4(0.5f, -0.3f, 0.0f, 1.0f); // lower right vertex
-		output.f4Position = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
+		output.f4Color = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
 	}
 	else if (id == 2)
 	{
 		output.f4Position = float4(0.0f, 0.5f, 0.0f, 1.0f); // top vertex
-		output.f4Position = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
+		output.f4Color = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
 	}
 	if (id == 3) // Triangle 2
 	{
 		output.f4Position = float4(-0.4f, 0.1f, 0.0f, 1.0f); // Lower left vertex
-		output.f4Position = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
+		output.f4Color = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
 	}
 	else if (id == 4)
 	{
 		output.f4Position = float4(0.4f, 0.1f, 0.0f, 1.0f); // lower right vertex
-		output.f4Position = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
+		output.f4Color = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
 	}
 	else if (id == 5)
 	{
 		output.f4Position = float4(0.0f, 0.8f, 0.0f, 1.0f); // top vertex
-		output.f4Position = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
+		output.f4Color = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
 	}
 	if (id == 6) // Triangle 3
 	{
 		output.f4Position = float4(-0.6f, -0.8f, 0.0f, 1.0f); // Lower left vertex
-		output.f4Position = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
+		output.f4Color = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
 	}
 	else if (id == 7)
 	{
 		output.f4Position = float4(0.6f, -0.8f, 0.0f, 1.0f); // lower right vertex
-		output.f4Position = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
+		output.f4Color = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
 	}
 	else if (id == 8)
 	{
 		output.f4Position = float4(0.0f, 0.3f, 0.0f, 1.0f); // top vertex
-		output.f4Position = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
+		output.f4Color = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
 	}
 	return output;
 }
