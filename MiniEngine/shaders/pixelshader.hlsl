14a596b4a5204b7f74a492ecc78f06daeb2244b9
Jonas Laptop <jonas.erkert@t-online.de>
2020-01-20 10:22:16 +0100

    Created advanced vertex- and pixelshader

--- MiniEngine/shaders/pixelshader.hlsl
+++ MiniEngine/shaders/pixelshader.hlsl
@@ -1,10 +1,37 @@
 struct PS_Input
 {
-	float4 f4Position : SV_Position;
-	float4 f4Color : ASFD;
+	float4 f4Position:			SV_Position;
+	float4 f4PosWorld:			POSITION;
+	float4 f4Color :			ASFD;
+	float4 f4Normal:			NORMAL;
+	float4 fCamPos:				POSITION2;
+	float4 f4LightDirection:	NORMAL2;
 };
+
+cbuffer ColorChanging : register(b1)
+{
+	matrix mColorChanging: packoffset(c0);	// 4 Bytes * 16 = 64 Bytes
+	matrix mPad1: packoffset(c4);			// 4 Bytes * 16 = 64 Bytes
+	matrix mPad2: packoffset(c8);			// 4 Bytes * 16 = 64 Bytes
+	matrix mPad3: packoffset(c12);			// 4 Bytes * 16 = 64 Bytes
+	// Sum: 256 Bytes
+}
 
 float4 main(PS_Input input) : SV_TARGET
 {
+	// Fractional (0...1) light amount from the original quantity
+	// Phong-Shading
+	float fShadingForce = mul(input.f4Normal, -input.f4LightDirection);
+
+	// Apply color changing matrix for color animation
+	input.f4Color = mul(input.f4Color, mColorChanging);
+
+	// Clamp color values
+	input.f4Color = saturate(input.f4Color);
+
+	// Weight color
+	input.f4Color *= fShadingForce;
+
+	// Return weighted color to the output merger
 	return input.f4Color;
 }
--- MiniEngine/shaders/vertexshader.hlsl
+++ MiniEngine/shaders/vertexshader.hlsl
@@ -9,57 +9,58 @@ struct VS_Input
 
 struct VS_Output
 {
-	float4 f4Position : SV_Position;
-	float4 f4Color : ASFD;
+	float4 f4Position		: SV_Position;
+	float4 f4PosWorld		: POSITION;
+	float4 f4Color			: ASFD;
+	float4 f4Normal			: NORMAL;
+	float4 f4CamPos			: POSITION2;
+	float4 f4LightDirection	: NORMAL2;
 };
 
-VS_Output main(uint id : SV_VertexID)
+cbuffer ObjectInfo : register(b0)
+{
+	matrix mWorld:		packoffset(c0);			// 4 Bytes * 16 = 64 Bytes
+	matrix mView:		packoffset(c4);			// 4 Bytes * 16 = 64 Bytes
+	matrix mProj:		packoffset(c8);			// 4 Bytes * 16 = 64 Bytes
+	float4 f4CamPos:	packoffset(c12);		// 4 Bytes * 4 = 16 Bytes
+	float4 f4LightDirection: packoffset(c13);	// 4 Bytes * 4 = 16 Bytes
+	float4 f4Pad1:		packoffset(c14);		// 4 Bytes * 4 = 16 Bytes
+	float4 f4Pad2s:		packoffset(c15);		// 4 Bytes * 4 = 16 Bytes
+	// Sum: 256 Bytes
+}
+
+VS_Output main(VS_Input input)
 {
 	VS_Output output;
-	if (id == 0)
-	{
-		output.f4Position = float4(-0.5f, -0.3f, 0.0f, 1.0f); // Lower left vertex
-		output.f4Color = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
-	}
-	else if (id == 1)
-	{
-		output.f4Position = float4(0.5f, -0.3f, 0.0f, 1.0f); // lower right vertex
-		output.f4Color = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
-	}
-	else if (id == 2)
-	{
-		output.f4Position = float4(0.0f, 0.5f, 0.0f, 1.0f); // top vertex
-		output.f4Color = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
-	}
-	if (id == 3) // Triangle 2
-	{
-		output.f4Position = float4(-0.4f, 0.1f, 0.0f, 1.0f); // Lower left vertex
-		output.f4Color = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
-	}
-	else if (id == 4)
-	{
-		output.f4Position = float4(0.4f, 0.1f, 0.0f, 1.0f); // lower right vertex
-		output.f4Color = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
-	}
-	else if (id == 5)
-	{
-		output.f4Position = float4(0.0f, 0.8f, 0.0f, 1.0f); // top vertex
-		output.f4Color = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
-	}
-	if (id == 6) // Triangle 3
-	{
-		output.f4Position = float4(-0.6f, -0.8f, 0.0f, 1.0f); // Lower left vertex
-		output.f4Color = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
-	}
-	else if (id == 7)
-	{
-		output.f4Position = float4(0.6f, -0.8f, 0.0f, 1.0f); // lower right vertex
-		output.f4Color = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
-	}
-	else if (id == 8)
-	{
-		output.f4Position = float4(0.0f, 0.3f, 0.0f, 1.0f); // top vertex
-		output.f4Color = float4(0.0f, 0.7f, 0.0f, 1.0f); // Green
-	}
+	
+	// Convert vector3 into vector4
+	float4 f4PosInput;
+	f4PosInput.xyz = input.f3Pos;
+	f4PosInput.w = 1.0f;
+
+	// World transformation
+	output.f4Position = mul(f4PosInput, mWorld);
+
+	output.f4PosWorld = output.f4Position;
+
+	// Unify view- and projection matrices 
+	matrix mViewProj = mul(mView, mProj);
+
+	// Project points correctly and consider camera position when calculating
+	output.f4Position = mul(output.f4Position, mViewProj);
+
+	// Calculate normals
+	output.f4Normal.xyz = input.f3Normal;
+	output.f4Normal.w = 0.0f; // Direction vector => 0
+	output.f4Normal = mul(output.f4Normal, mWorld);
+
+	// Camera pos
+	output.f4CamPos = f4CamPos;
+
+	// Light dir
+	output.f4LightDirection = f4LightDirection;
+	
+	output.f4Color = float4(0.0f, 1.0f, 0.0f, 1.0f); // Green, no transparency
+
 	return output;
 }
